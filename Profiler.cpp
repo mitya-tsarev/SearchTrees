67fff834640649693d3abbfa34770532358e52ba
Mitya Tsarev <mitya.tsarev@gmail.com>
2020-11-14 08:14:32 +0300

    Debug profiler

--- Profiler.cpp
+++ Profiler.cpp
@@ -5,131 +5,145 @@
 #include <fstream>
 #include <string>
 #include "AVLTree.h"
-#include "RBTree.h"                    
+#include "RBTree.h"
+
 using namespace std;
- 
- struct times{ // struct containing number of nodes in the tree and average operation execution times for it
- 	int n;
- 	double timeinsert;
- 	double timefind;
- 	double timeerase;
- };
 
-class Profiler: public AVL_Tree, public RB_Tree{
-
-private
-
-	string s;
-	
-public:
-	
-	Profiler(s1){
-		s = s1;
-	}
-	
-	~Profiler(){
-		
-	}
-	
-	vector<times> average(int x1, int x2, int x3, int x4){
-
-		srand(time(0));
-    
-    	double start;
-    
-    	if (s == "AVL_Tree"){
-    		AVL_tree<int> avl = AVL_tree<int>();
-		}
-		else if (s == "RB_Tree"){
-			RB_Tree<int> avl = RB_Tree<int>();
-		}
-    	
-    	
-    	int array_size; // number of nodes
-    	vector<times> plottimes; // vector where data will be stored
-    	times dot;
-    	int test;
-    	
-    	double timeinsertn = 0;
-    	double timefindn = 0;
-    	double timeerasen = 0;
-    	double timeinsert1 = 0;
-	    double timefind1 = 0;
-	    double timeerase1 = 0;
-	    double timeinsert2 = 0;
-	    double timefind2 = 0;
-	    double timeerase2 = 0;
-    
-    	for(int i = 1; i <= y1; i++){ // search for average operation execution times for trees with different number of nodes (y1)
-    		array_size = i*1000000000;
-    		timeinsertn = 0;
-    		timefindn = 0;
-    		timeerasen = 0;
-    		for (int k = 0; k < y2; k++){ // search for average operation execution times for different trees with the same number of nodes (y2)
-	    		avl = AVL_tree<int>(); // create new tree
-	    		for (int j = 0; j < array_size; j++)
-	    		{
-	    			avl.insert(((rand() % 100000)*100000 + rand() % 10000) % (array_size*10)); // add nodes with random values  			
-	    		}    
-	    		timeinsert1 = 0;
-	    		timefind1 = 0;
-	    		timeerase1 = 0;
-	    		for (int j = 0; j < x1; j++){ // search for average operation execution times for different numbers (x1)
-		    		test = ((rand() % 10000)*10000 + rand() % 10000) % (array_size*10);
-		    		cout << test << endl;
-		    		timeinsert2 = 0;
-	    			timefind2 = 0;
-	    			timeerase2 = 0;
-		    		for (int l = 0; l < x2; l++){ // search for average operation execution times for the same number (x2)
-		    			start = clock();
-		    			avl.insert(test);
-		    			timeinsert2 += clock()*10000 - start*10000;
-		    			start = clock();
-		    			avl.find(test);
-	    				timefind2 += clock()*10000 - start*10000;
-	    				start = clock();
-	    				avl.erase(test);
-	    				timeerase2 += clock()*10000 - start*10000;
-					}
-					timeinsert2 = timeinsert2 / x2;
-					timefind2 = timefind2 / x2;
-					timeerase2 = timeerase2 / x2;
-	    			timeinsert1 += timeinsert2;
-	   				timefind1 += timefind2; 
-	   				timeerase1 += timeerase2; 
-				}
-				timeinsert1 = timeinsert1 / x1;
-				timefind1 = timefind1 / x1; 
-	    		timeerase1 = timeerase1 / x1; 
-				timeinsertn += timeinsert1;
-	    		timefindn += timefind1; 
-	    		timeerasen += timeerase1; 
-			}
-			timeinsertn = timeinsertn / y2;
-	    	timefindn = timefindn / y2; 
-	    	timeerasen = timeerasen / y2; 
-			dot.n = array_size;
-	    	dot.timeinsert = timeinsertn;
-	    	dot.timefind = timefindn;
-	    	dot.timeerase = timeerasen;
-	    	plottimes.push_back(dot);
-		}
-	}
+struct times { // struct containing number of nodes in the tree and average operation execution times for it
+    int n;
+    double timeinsert;
+    double timefind;
+    double timeerase;
 };
 
-int main(){
-	
-	ofstream out;
-	out.open("data.txt");
-	
-	Profiler prof = Profiler("AVL_Tree");
-    vector<times> a = prof.average(8, 8, 8, 1);
-	
-	for(int i = 0; i < y1; i++){
-		if (out.is_open()){
-			out << a[i].n << ' ' << a[i].timeinsert << ' ' << a[i].timefind << ' ' << a[i].timeerase << endl;
-		}			
-	}
-	out.close();
-	return 0;
+template <typename T>
+class Profiler : public AVLTree<T>, public RBTree<T> {
+
+private:
+
+    string s;
+
+public:
+
+    Profiler(std::string s1) {
+        s = s1;
+    }
+
+    ~Profiler() {
+
+    }
+
+    vector<times> average(int x1, int x2, int y1, int y2) {
+
+        srand(time(0));
+
+        double start;
+
+        if (s == "AVL_Tree") {
+            AVLTree<T> avl = AVLTree<T>();
+        } else if (s == "RB_Tree") {
+            RBTree<T> avl = RBTree<T>();
+        }
+
+
+        int array_size; // number of nodes
+        vector<times> plottimes; // vector where data will be stored
+        times dot;
+        int test;
+
+        double timeinsertn = 0;
+        double timefindn = 0;
+        double timeerasen = 0;
+        double timeinsert1 = 0;
+        double timefind1 = 0;
+        double timeerase1 = 0;
+        double timeinsert2 = 0;
+        double timefind2 = 0;
+        double timeerase2 = 0;
+
+        for (int i = 1; i <=
+                        y1; i++) { // search for average operation execution times for trees with different number of nodes (y1)
+            array_size = i * 1000000;
+            timeinsertn = 0;
+            timefindn = 0;
+            timeerasen = 0;
+            for (int k = 0; k <
+                            y2; k++) { // search for average operation execution times for different trees with the same number of nodes (y2)
+                AVLTree<int> avl = AVLTree<int>(); // create new tree
+                for (int j = 0; j < array_size; j++) {
+                    avl.insert(((rand() % 100000) * 100000 + rand() % 10000) %
+                               (array_size * 10)); // add nodes with random values
+                }
+                timeinsert1 = 0;
+                timefind1 = 0;
+                timeerase1 = 0;
+                for (int j = 0;
+                     j < x1; j++) { // search for average operation execution times for different numbers (x1)
+                    test = ((rand() % 10000) * 10000 + rand() % 10000) % (array_size * 10);
+                    cout << test << endl;
+                    timeinsert2 = 0;
+                    timefind2 = 0;
+                    timeerase2 = 0;
+                    for (int l = 0;
+                         l < x2; l++) { // search for average operation execution times for the same number (x2)
+                        start = clock();
+                        avl.insert(test);
+                        timeinsert2 += clock() * 10000 - start * 10000;
+                        start = clock();
+                        avl.find(test);
+                        timefind2 += clock() * 10000 - start * 10000;
+                        start = clock();
+                        avl.erase(test);
+                        timeerase2 += clock() * 10000 - start * 10000;
+                    }
+                    timeinsert2 = timeinsert2 / x2;
+                    timefind2 = timefind2 / x2;
+                    timeerase2 = timeerase2 / x2;
+                    timeinsert1 += timeinsert2;
+                    timefind1 += timefind2;
+                    timeerase1 += timeerase2;
+                }
+                timeinsert1 = timeinsert1 / x1;
+                timefind1 = timefind1 / x1;
+                timeerase1 = timeerase1 / x1;
+                timeinsertn += timeinsert1;
+                timefindn += timefind1;
+                timeerasen += timeerase1;
+            }
+            timeinsertn = timeinsertn / y2;
+            timefindn = timefindn / y2;
+            timeerasen = timeerasen / y2;
+            dot.n = array_size;
+            dot.timeinsert = timeinsertn;
+            dot.timefind = timefindn;
+            dot.timeerase = timeerasen;
+            plottimes.push_back(dot);
+        }
+        return plottimes;
+    }
+};
+
+int main() {
+    /*RBTree<int> rb = RBTree<int>();
+    rb.insert(5);
+    rb.insert(3);
+    rb.insert(179);
+    //rb.insert(1);
+    std::cout << "Searching RB tree: \n179: " << (rb.find(179) ? "Yes" : "No") << "\n40: " << (rb.find(40) ? "Yes" : "No") << '\n';*/
+
+    ofstream out;
+    out.open("data.txt");
+    int y1 = 1;
+
+    Profiler<int> prof = Profiler<int>("AVL_Tree");
+    vector<times> a = prof.average(1, 1, 1, 1);
+
+    for (int i = 0; i < y1; i++) {
+        if (out.is_open()) {
+            out << a[i].n << ' ' << a[i].timeinsert << ' ' << a[i].timefind << ' ' << a[i].timeerase << endl;
+        }
+    }
+    out.close();
+    return 0;
 }
+
